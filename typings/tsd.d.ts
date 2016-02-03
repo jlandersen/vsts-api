6fa9428b8741855a4ea3d5bcca44c94faf4432ff
Jeppe Andersen <jeppe@outlook.com>
2016-02-03 18:40:57 +0100

    Move to ES5 from ES6 to support Node.js 0.12
    - Transpile to ES5
    - Test Node.js 0.12 on Travis

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="jasmine/jasmine.d.ts" />
 /// <reference path="node/node.d.ts" />
 /// <reference path="restler/restler.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
